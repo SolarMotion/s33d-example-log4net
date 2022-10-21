46c34199cc9418b7b8a321276345e1cd97a49e87
weichien <ywc92@hotmail.com>
2022-10-22 02:20:57 +0800

    first commit

---
+++ Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using ServiceStack;
+using static Console.Utils;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var user = new UserModel() { ID = 5, IsActive = true, Name = "User A" };
+
+                var result = user.ToJson();
+
+                LogApiInfo("This is web api");
+
+                var result2 = result.FromJson<UserModel>();
+
+                WriteLine(result2.Name);
+            }
+            catch (Exception ex)
+            {
+                LogWebError(ex);
+            }
+            finally
+            {
+                WriteLine("Any key to exit");
+                Read();
+            }
+        }
+    }
+}
---
+++ Console/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+
+namespace Console
+{
+    internal static class Utils
+    {
+        private static readonly log4net.ILog _webApiLogger = log4net.LogManager.GetLogger("WebApiLogger");
+        private static readonly log4net.ILog _webLogger = log4net.LogManager.GetLogger("WebLogger");
+
+        internal static void LogApiInfo(string message)
+        {
+            _webApiLogger.Info(message);
+        }
+
+        internal static void LogApiError(Exception ex)
+        {
+            _webApiLogger.Error(ex);
+        }
+
+        internal static void LogWebInfo(string message)
+        {
+            _webLogger.Info(message);
+        }
+
+        internal static void LogWebError(Exception ex)
+        {
+            _webLogger.Error(ex);
+        }
+    }
+}
